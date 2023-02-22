dc5dd4b31c1e12b6a268f88c550cdfa1ab5d6127
gagan goyal <goyalgagan82@gmail.com>
2023-02-22 10:30:48 +0530

    Add Expenses js css

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import './ExpenseDate.css'
+
 function ExpenseDate(props1) {
   const month = props1.date.toLocaleString("en-Us", { month: 'long' });
   const day = props1.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props1.date.getFullYear();
 
   return (
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
       </div>
   );
 }
